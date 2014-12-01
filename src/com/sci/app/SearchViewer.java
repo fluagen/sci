26d705ac68fdc247bde2779e9cbdef921319bcee
calis <calis@majie>
2014-12-01 18:46:32 +0800

    add mvc

--- src/com/sci/app/SearchViewer.java
+++ src/com/sci/app/SearchViewer.java
@@ -1,17 +1,22 @@
 package com.sci.app;
 
+import java.awt.Component;
 import java.awt.Container;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
 
 import com.sci.app.toolkit.DBType;
+import com.sci.app.toolkit.UIIdentity;
 import com.sci.app.view.ICondView;
 import com.sci.app.view.impl.CondView;
 
@@ -30,7 +35,7 @@ public class SearchViewer extends JFrame{
 		init();
 	}
 	public void init(){
-		Container contentPane = getContentPane();
+		final Container contentPane = getContentPane();
 		contentPane.setLayout(new GridBagLayout());
 		GridBagConstraints gbc = new GridBagConstraints();
 		gbc.insets = new Insets(15, 10, 15, 10);//设置组件之间彼此的间距
@@ -38,6 +43,7 @@ public class SearchViewer extends JFrame{
 		//数据库选择
 		JLabel dbLabel = new JLabel("数据库：");
 		JComboBox dbBox = new JComboBox(DBType.getDBTypes());
+		dbBox.setName(UIIdentity.DB_COMBOBOX);
 		
 		gbc.gridwidth = 1;
 		gbc.weightx = 0;
@@ -54,25 +60,51 @@ public class SearchViewer extends JFrame{
 		
 		ICondView condView = new CondView();
 		JPanel condPanel = condView.getJPanel();
-		gbc.gridx = 0;
-		gbc.gridy = 1;
+//		gbc.gridx = 0;
+//		gbc.gridy = 1;
 		gbc.gridwidth = 0;
-		gbc.gridheight = 0;
+		gbc.gridheight = 1;
 		gbc.weightx = 1;
 		gbc.fill = GridBagConstraints.BOTH;
 		gbc.weighty = 1;
 		contentPane.add(condPanel,gbc);
 		
-//		JPanel bottomPanel = new JPanel();
-//		bottomPanel.setBorder(BorderFactory.createTitledBorder("bottom"));
-//		gbc.fill = GridBagConstraints.BOTH;
-//		gbc.gridwidth = 0;
-//		gbc.weightx = 1;
-//		gbc.weighty = 1;
-//		contentPane.add(bottomPanel,gbc);
 		
+		JPanel bottomPanel = new JPanel();
+		gbc.gridwidth = 0;
+		gbc.gridheight = 1;
+		gbc.weighty = 0;
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+		JButton btn = new JButton("检索");
+		btn.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				Component[]  coms = contentPane.getComponents();
+				for(int i=0;i<coms.length;i++){
+					Component component = coms[i];
+					if(component.getClass().getName().equals("javax.swing.JComboBox")){
+						JComboBox box = (JComboBox) component;
+						System.out.println(box.getSelectedItem().toString());
+					}
+					if(component.getClass().getName().equals("javax.swing.JPanel")){
+						JPanel pnl = (JPanel) component;
+						Component[]  subcoms = pnl.getComponents();
+						for(int j=0;j<subcoms.length;j++){
+							Component subcomponent = subcoms[j];
+							if(subcomponent.getName() != null && subcomponent.getName().equals(UIIdentity.LANGUAGE_LIST)){
+								System.out.println(((JList)subcomponent).getSelectedValue());
+							}
+						}
+						
+					}
+				}
+			}
+		});
 		
+		bottomPanel.add(btn);
 		
+		contentPane.add(bottomPanel,gbc);
 		
 		this.setVisible(true);
 	}
--- src/com/sci/app/view/impl/CondView.java
+++ src/com/sci/app/view/impl/CondView.java
@@ -1,16 +1,23 @@
 package com.sci.app.view.impl;
 
-import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Insets;
 
 import javax.swing.BorderFactory;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.ListSelectionModel;
 
+import com.sci.app.toolkit.DocType;
+import com.sci.app.toolkit.Language;
 import com.sci.app.toolkit.Timespan;
+import com.sci.app.toolkit.UIIdentity;
 import com.sci.app.view.ICondView;
 
 public class CondView implements ICondView{
@@ -26,6 +33,10 @@ public class CondView implements ICondView{
 			panel.setLayout(new GridBagLayout());
 			
 			drawTimespan(panel);
+			drawLanguage(panel);
+			drawDoctype(panel);
+			drawExps(panel);
+			drawBottom(panel);
 			
 		}
 		return panel;
@@ -33,10 +44,12 @@ public class CondView implements ICondView{
 	
 	public static void drawTimespan(JPanel panel){
 		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.insets = new Insets(5, 5, 5, 5);
 		JLabel timespan = new JLabel("时间跨度：");
+		timespan.setPreferredSize(new Dimension(100,10));
 		gbc.fill = GridBagConstraints.BOTH;
-		gbc.anchor = GridBagConstraints.NORTHWEST;
-		gbc.gridwidth = 2;
+//		gbc.anchor = GridBagConstraints.NORTHWEST;
+		gbc.gridwidth = 1;
 		gbc.gridheight = 1;
 		gbc.weightx = 0;
 		panel.add(timespan,gbc);
@@ -47,12 +60,16 @@ public class CondView implements ICondView{
 //		gbc.weighty = 0;
 		
 		JLabel from = new JLabel("从");
+//		from.setPreferredSize(new Dimension(30,10));
 		panel.add(from,gbc);
 		JComboBox fComboBox = new JComboBox(Timespan.getTimespan());
+		fComboBox.setName(UIIdentity.FROM_TIME_COMBOBOX);
 		panel.add(fComboBox,gbc);
 		JLabel to = new JLabel("到");
+//		to.setPreferredSize(new Dimension(30,10));
 		panel.add(to,gbc);
 		JComboBox tComboBox = new JComboBox(Timespan.getTimespan());
+		tComboBox.setName(UIIdentity.TO_TIME_COMBOBOX);
 		panel.add(tComboBox,gbc);
 		
 		JPanel strut = new JPanel();
@@ -61,6 +78,105 @@ public class CondView implements ICondView{
 		gbc.weighty = 0;
 		panel.add(strut,gbc);
 		
+	}
+	
+	public static void drawLanguage(JPanel panel){
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.insets = new Insets(10, 10, 10, 10);
+		JLabel label = new JLabel("语种：");
+		
+		gbc.fill = GridBagConstraints.BOTH;
+		gbc.gridwidth = 1;
+		gbc.gridheight = 1;
+		gbc.weightx = 0;
+		panel.add(label,gbc);
+		
+		JList list = new JList(Language.getLanguages());
+		list.setName(UIIdentity.LANGUAGE_LIST);
+		//list.setPreferredSize(new Dimension(200,80));
+		list.setVisibleRowCount(5);
+		list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		JScrollPane scroll = new JScrollPane(list);
+		scroll.setPreferredSize(new Dimension(150, 80));
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+		gbc.gridwidth = 5;
+		gbc.weightx = 1;
+		panel.add(scroll,gbc);
+		
+		JPanel strut = new JPanel();
+		strut.setPreferredSize(new Dimension(150,80));
+		gbc.gridwidth = 0;
+		gbc.weightx = 1;
+		gbc.weighty = 0;
+		panel.add(strut,gbc);
+		
+	}
+	
+	public static void drawDoctype(JPanel panel){
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.insets = new Insets(10, 10, 10, 10);
+		
+		JLabel label = new JLabel("文献类型：");
+		gbc.fill = GridBagConstraints.BOTH;
+		gbc.gridwidth = 1;
+		gbc.gridheight = 1;
+		gbc.weightx = 0;
+		panel.add(label,gbc);
+		
+		JList list = new JList(DocType.getDocTypes());
+		list.setName(UIIdentity.DOCTYPE_LIST);
+		//list.setPreferredSize(new Dimension(200,80));
+		list.setVisibleRowCount(5);
+		list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		JScrollPane scroll = new JScrollPane(list);
+		scroll.setPreferredSize(new Dimension(150, 80));
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+		gbc.gridwidth = 5;
+		gbc.weightx = 1;
+		panel.add(scroll,gbc);
+		
+		JPanel strut = new JPanel();
+		strut.setPreferredSize(new Dimension(150,20));
+		gbc.gridwidth = 0;
+		gbc.weightx = 1;
+		gbc.weighty = 0;
+		panel.add(strut,gbc);
+	}
+	
+	public static void drawExps(JPanel panel){
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.insets = new Insets(10, 10, 10, 10);
+		
+		JLabel label = new JLabel("检索表达式：");
+		gbc.fill = GridBagConstraints.BOTH;
+		gbc.gridwidth = 1;
+		gbc.gridheight = 1;
+		gbc.weightx = 0;
+		panel.add(label,gbc);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setName(UIIdentity.EXPS_TEXTAREA);
+		//textArea.setRows(10);
+		textArea.setLineWrap(true); // 激活自动换行功能
+		textArea.setWrapStyleWord(true);
+		JScrollPane scroll = new JScrollPane(textArea);
+		scroll.setPreferredSize(new Dimension(150, 80));
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+		gbc.gridwidth = 0;
+		gbc.weightx = 1;
+		panel.add(scroll,gbc);
+	}
+	
+	public static void drawBottom(JPanel panel){
+		GridBagConstraints gbc = new GridBagConstraints();
+		
+		JPanel bottom = new JPanel();
+		gbc.fill = GridBagConstraints.BOTH;
+		gbc.gridwidth = 0;
+		gbc.gridheight = 0;
+		gbc.weightx = 1;
+		gbc.weighty = 1;
+		panel.add(bottom,gbc);
 	}
 	
 	
